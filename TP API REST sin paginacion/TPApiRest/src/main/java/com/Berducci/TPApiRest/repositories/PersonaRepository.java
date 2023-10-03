7d267149ecc9b4b96e0c3d736c62c6aa164bbc7e
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-10-03 20:20:29 -0300

    Delete TP API REST sin paginacion directory

--- TP API REST sin paginacion/TPApiRest/src/main/java/com/Berducci/TPApiRest/repositories/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Berducci.TPApiRest.repositories;
-
-import com.Berducci.TPApiRest.entities.Persona;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends BaseRepository <Persona, Long>{
-}
